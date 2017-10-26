bdf81c8231577cc6c3d98db161507c4b27a192a0
change_your_name <change_your_email_adress@medieninf.de>
2017-10-26 10:28:21 +0200

    rot13

---
+++ Rot13/rot13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int rot13(int c){
+    if(c >='A' && c<='Z'){
+        return ((((c-'A')+13)%26)+'A');
+    }else if(c>='a' && c<='z'){
+        return ((((c-'a')+13)%26)+'a');
+    }
+    
+    return c;
+}
+
+int main(void){
+    int c;
+    
+    while((c = getchar()) != EOF){
+        putchar(rot13(c));
+    }
+    
+    return 0;
+}
